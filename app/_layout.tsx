9bded9bdac0f7e532adcca7ae092b04b3de89332
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-12 12:39:26 -0400

    Add Tailwind CSS configuration and NativeWind typings
    - Added Tailwind CSS configuration file (tailwind.config.js) to customize the theme and extend the default styles.
    - Included NativeWind typings in the project by adding a new file (nativewind-env.d.ts) with a reference to the types.
    - Updated the tsconfig.json file to include the new NativeWind typings file in the list of files to be compiled.
    - Ignored the generated expo-env.d.ts file in the .gitignore file.
    - Updated the metro.config.js file to use NativeWind with the global.css file as input.
    - Modified the babel.config.js file to include the babel-preset-expo and nativewind/babel presets.
    - Deleted the ThemedText-test.tsx and ThemedText-test.tsx.snap files from the __tests__ folder.
    - Updated the package.json file to include the nativewind and tailwindcss dependencies.
    - Deleted the ThemedView.tsx, TabBarIcon.tsx, ExternalLink.tsx, NotFoundScreen.tsx, HelloWave.tsx, and Colors.ts files.
    - Updated the ThemedText.tsx file to include the ThemedText component with different text styles.
    - Updated the ThemedView.tsx file to include the ThemedView component with customizable background color and safe area handling.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text, View } from 'react-native';
+
+import '../global.css';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedView from '@/presentation/shared/ThemedView';
+import ThemedText from '@/presentation/shared/ThemedText';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, 'background');
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -27,11 +40,18 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GestureHandlerRootView
+      style={{ backgroundColor: backgroundColor, flex: 1 }}
+    >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <ThemedText className="mt-20">Hola Mundo</ThemedText>
+        </ThemedView>
+
+        {/* <Stack>
+        
+      </Stack> */}
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
