d616de6daaa0e22f268c421a952420b82be94243
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-12 13:09:24 -0400

    Refactor menu rendering and add new menu items

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,9 @@
-import { animationMenuRoutes } from '@/constants/Routes';
+import {
+  animationMenuRoutes,
+  menuRoutes,
+  uiMenuRoutes,
+} from '@/constants/Routes';
+import MenuItem from '@/presentation/menu/MenuItem';
 import ThemedView from '@/presentation/shared/ThemedView';
 import { Href, Link } from 'expo-router';
 import { View, Text } from 'react-native';
@@ -6,7 +11,40 @@ const ComponentsApp = () => {
   return (
     <ThemedView margin>
       {animationMenuRoutes.map((route, index) => (
-        <Link href={route.name.split('/')[0] as Href}>{route.title}</Link>
+        <MenuItem
+          key={route.title}
+          title={route.title}
+          icon={route.icon}
+          name={route.name}
+          isFirst={index === 0}
+          isLast={index === animationMenuRoutes.length - 1}
+        />
+      ))}
+
+      <View className="my-3" />
+
+      {uiMenuRoutes.map((route, index) => (
+        <MenuItem
+          key={route.title}
+          title={route.title}
+          icon={route.icon}
+          name={route.name}
+          isFirst={index === 0}
+          isLast={index === uiMenuRoutes.length - 1}
+        />
+      ))}
+
+      <View className="my-3" />
+
+      {menuRoutes.map((route, index) => (
+        <MenuItem
+          key={route.title}
+          title={route.title}
+          icon={route.icon}
+          name={route.name}
+          isFirst={index === 0}
+          isLast={index === menuRoutes.length - 1}
+        />
       ))}
     </ThemedView>
   );
