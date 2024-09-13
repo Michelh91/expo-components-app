2bc583b436efc9ef075dfdaeee7124decac0f29f
Fernando Herrera <fernando.herrera85@gmail.com>
2024-09-13 11:28:11 -0400

    Refactor menu rendering and add new menu items

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native';
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 const Animation101Screen = () => {
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedButton className="mb-5" onPress={() => console.log('fadeIn')}>
+        FadeIn
+      </ThemedButton>
+
+      <ThemedButton className="mb-5" onPress={() => console.log('fadeOut')}>
+        FadeOut
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
